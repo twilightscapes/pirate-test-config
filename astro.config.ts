a23651ad7acefd97f101e084937e5db99d0110ad
Todd <ihatespam@gmail.com>
2024-09-12 03:47:44 -0500

    revert to pre agnostic

--- astro.config.ts
+++ astro.config.ts
@@ -13,85 +13,52 @@ import AstroPWA from '@vite-pwa/astro';
 import markdoc from "@astrojs/markdoc";
 import keystatic from '@keystatic/astro';
 import netlify from "@astrojs/netlify";
-import vercel from '@astrojs/vercel/serverless';
+
 import yaml from 'js-yaml';
 
-let pwaConfig: Record<string, any> = {};
-
-try {
-  const pwaSettingsFile = import.meta.glob('./src/content/pwaSettings/index.yaml', { query: '?raw', import: 'default', eager: true });
-  const pwaConfigYaml = Object.values(pwaSettingsFile)[0] as string;
-  if (typeof pwaConfigYaml !== 'string') {
-    throw new Error('pwaConfigYaml must be a string');
-  }
-  pwaConfig = yaml.load(pwaConfigYaml) as Record<string, any>;
-} catch (error) {
-  console.error('Error loading PWA configuration:', error);
-  // Provide default values if the file cannot be read
-  pwaConfig = {
-    siteUrl: 'https://default-site-url.com',
-    name: 'Default Name',
-    shortName: 'Default Short Name',
-    description: 'Default Description',
-    themeColor: '#ffffff',
-    backgroundColor: '#ffffff',
-    startUrl: '/',
-    display: 'standalone',
-    icon192: '/default-icon-192.png',
-    icon512: '/default-icon-512.png',
-  };
+const pwaSettingsFile = import.meta.glob('./src/content/pwaSettings/index.yaml', { query: '?raw', import: 'default', eager: true });
+const pwaConfigYaml = Object.values(pwaSettingsFile)[0] as string;
+const pwaConfig = yaml.load(pwaConfigYaml) as Record<string, any>;
+if (typeof pwaConfigYaml !== 'string') {
+  throw new Error('pwaConfigYaml must be a string');
 }
-
-// Determine the adapter and output based on the environment
-const isVercel = !!process.env.VERCEL;
-const adapter = isVercel ? vercel() : netlify();
-const output = isVercel ? 'server' : 'hybrid';
-
-console.log(`Using adapter: ${isVercel ? 'Vercel' : 'Netlify'}`);
-console.log(`Output mode: ${output}`);
 
 export default defineConfig({
   image: {
     domains: ["webmention.io"]
   },
-  integrations: [
-    AstroPWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['robots.txt', 'manifest.webmanifest'],
-      manifest: {
-        id: pwaConfig.startUrl,
-        name: pwaConfig.name,
-        short_name: pwaConfig.shortName,
-        description: pwaConfig.description,
-        theme_color: pwaConfig.themeColor,
-        start_url: pwaConfig.startUrl,
-        background_color: pwaConfig.backgroundColor,
-        display: pwaConfig.display,
-        icons: [{
-          src: pwaConfig.icon192,
-          sizes: '192x192',
-          type: 'image/png'
-        }, {
-          src: pwaConfig.icon512,
-          sizes: '512x512',
-          type: 'image/png'
-        }]
-      },
-      workbox: {
-        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
-      }
-    }),
-    mdx(),
-    react(),
-    icon(),
-    tailwind({
-      applyBaseStyles: false
-    }),
-    sitemap(),
-    keystatic(),
-    markdoc()
-  ],
-  markdown: {
+  integrations: [mdx(), react(), icon(), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), keystatic(), 
+  
+  AstroPWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['robots.txt', 'manifest.webmanifest'],
+    manifest: {
+      id: pwaConfig.startUrl,
+      name: pwaConfig.name,
+      short_name: pwaConfig.shortName,
+      description: pwaConfig.description,
+      theme_color: pwaConfig.themeColor,
+      start_url: pwaConfig.startUrl,
+      background_color: pwaConfig.backgroundColor,
+      display: pwaConfig.display,
+      icons: [{
+        src: pwaConfig.icon192,
+        sizes: '192x192',
+        type: 'image/png'
+      }, {
+        src: pwaConfig.icon512,
+        sizes: '512x512',
+        type: 'image/png'
+      }]
+    },
+    workbox: {
+      maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
+    }
+  }), 
+  
+  markdoc()],  markdown: {
     rehypePlugins: [[rehypeExternalLinks, {
       rel: ["nofollow", "noopener", "noreferrer"],
       target: "_blank"
@@ -106,7 +73,7 @@ export default defineConfig({
       theme: 'dracula',
     },
   },
-  output: output,
+  output: 'hybrid',
   prefetch: true,
   site: pwaConfig.siteUrl,
   redirects: {
@@ -124,7 +91,7 @@ export default defineConfig({
     },
     plugins: [rawFonts([".ttf", ".woff"])],
   },
-  adapter: adapter
+  adapter: netlify()
 });
 
 function rawFonts(ext: string[]) {
