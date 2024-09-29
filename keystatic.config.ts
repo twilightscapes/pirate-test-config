d12ca4b942fd4ed64e7091dc12b633386121924e
Todd <ihatespam@gmail.com>
2024-09-29 13:28:38 -0500

    Update keystatic.config.ts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -14,7 +14,7 @@ export default config({
     ? { kind: 'cloud' }
     : { kind: 'local' },
   cloud: isProduction
-    ? { project: process.env.KEYSTATIC_PROJECT || 'goto/keystatic-dot-cloud/' }
+    ? { project: process.env.KEYSTATIC_PROJECT || '' }
     : undefined,
   collections: {
     posts: collection({
